b8260b5d8508974eb520af066b818ce7527b5016
EmanuelBoscacci <131210440+EmanuelBoscacci@users.noreply.github.com>
2026-05-13 16:33:17 -0300

    Delete WebApplicationAnalisisNumerico directory

--- WebApplicationAnalisisNumerico/WebApplicationAnalisisNumerico/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplicationAnalisisNumerico.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
